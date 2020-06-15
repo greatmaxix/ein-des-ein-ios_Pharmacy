c95227b6579ba119606a16a37593866ad6bc1ee5
Mishko <valisrevenge@gmail.com>
2020-06-15 11:35:20 +0300

    Added xcode project.

---
+++ Pharmacy/Pharmacy/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  Pharmacy
+//
+//  Created by CGI-Kite on 15.06.2020.
+//  Copyright © 2020 pharmacy. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+}
+
---
+++ Pharmacy/Pharmacy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Pharmacy
+//
+//  Created by CGI-Kite on 15.06.2020.
+//  Copyright © 2020 pharmacy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
