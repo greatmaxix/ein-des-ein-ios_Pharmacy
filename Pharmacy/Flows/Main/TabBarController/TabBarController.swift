2c4d65998ce2fa217265c52e03e8ae323fbcae11
Mishko <valisrevenge@gmail.com>
2020-07-27 12:23:02 +0300

    Added shadow to tab bar.

--- Pharmacy/Flows/Main/TabBarController/TabBarController.swift
+++ Pharmacy/Flows/Main/TabBarController/TabBarController.swift
@@ -26,6 +26,7 @@ class TabBarController: UITabBarController {
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
+        setupTabBar()
         model.controllerDidBecomeVisible()
     }
     
@@ -35,6 +36,37 @@ class TabBarController: UITabBarController {
         model.controllerDidBecomeHidden()
     }
     
+    func setupTabBar() {
+        tabBar.shadowImage = UIImage()
+        tabBar.backgroundImage = UIImage()
+        
+        let roundedView: UIView = UIView()
+        roundedView.frame = CGRect(origin: .zero, size: CGSize(width: tabBar.frame.width, height: tabBar.frame.height * 2))
+        roundedView.backgroundColor = .white
+        roundedView.layer.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMinYCorner]
+        roundedView.layer.cornerRadius = 30
+        tabBar.addSubview(roundedView)
+        tabBar.sendSubviewToBack(roundedView)
+        
+        let shadowLayer = CALayer()
+        shadowLayer.frame = roundedView.frame
+        shadowLayer.position = roundedView.layer.position
+        
+        shadowLayer.cornerRadius = 30
+        shadowLayer.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMinYCorner]
+        
+        let shadowPath = UIBezierPath(roundedRect: tabBar.bounds, cornerRadius: 30)
+
+        shadowLayer.shadowPath = shadowPath.cgPath
+        shadowLayer.backgroundColor = UIColor.white.cgColor
+        
+        shadowLayer.shadowColor = UIColor(red: 0, green: 0.145, blue: 0.918, alpha: 0.06).cgColor
+        shadowLayer.shadowOpacity = 1
+        shadowLayer.shadowRadius = 10
+        shadowLayer.shadowOffset = CGSize(width: 0, height: -4)
+        
+        roundedView.layer.insertSublayer(shadowLayer, at: 0)
+    }
 }
 
 extension TabBarController: TabBarControllerInput {
