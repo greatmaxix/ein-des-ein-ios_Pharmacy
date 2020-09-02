3d643bd1452e045331f61c70a2eee22a6d4f0a7e
Sapa Denys <denys.sapa.sbit@gmail.com>
2020-09-02 16:19:40 +0300

    Container base implementation

--- Pharmacy/Flows/Onboarding/Container/OnboardingContainerViewController.swift
+++ Pharmacy/Flows/Onboarding/Container/OnboardingContainerViewController.swift
@@ -10,7 +10,74 @@ import UIKit
 
 class OnboardingContainerViewController: UIViewController {
 
+    // MARK: - Outlets
+    @IBOutlet private weak var containerView: UIView!
+    @IBOutlet private weak var nextButton: UIButton!
+    @IBOutlet private weak var skipButton: UIButton!
+
+    // MARK: - Properties
+    private var pageViewController: UIPageViewController!
+
+    // MARK: - Lifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        setupContainerView()
+    }
+}
+
+// MARK: - Private methods
+extension OnboardingContainerViewController {
+
+    private func setupContainerView() {
+        let pageViewController = UIPageViewController(transitionStyle: .scroll, navigationOrientation: .horizontal)
+        pageViewController.dataSource = self
+        guard let pageView = pageViewController.view else {
+            return
+        }
+
+        view.addSubview(pageView)
+        pageView.translatesAutoresizingMaskIntoConstraints = false
+
+        NSLayoutConstraint.activate([pageView.topAnchor.constraint(equalTo: view.topAnchor),
+                                     pageView.leftAnchor.constraint(equalTo: view.leftAnchor),
+                                     pageView.rightAnchor.constraint(equalTo: view.rightAnchor),
+                                     pageView.bottomAnchor.constraint(equalTo: view.bottomAnchor)])
+
+        self.pageViewController = pageViewController
+        addChild(pageViewController)
+        pageViewController.willMove(toParent: self)
+        
+        setFirstViewController()
+    }
+
+    private func setFirstViewController() {
+
+    }
+}
+
+// MARK: - Actions
+extension OnboardingContainerViewController {
+
+    @IBAction private func onNextButtonTouchUp(_ sender: UIButton) {
+
+    }
+
+    @IBAction private func onSkipButtonTouchUp(_ sender: UIButton) {
+
+    }
+}
+
+// MARK: - UIPageViewControllerDataSource
+extension OnboardingContainerViewController: UIPageViewControllerDataSource {
+
+    func pageViewController(_ pageViewController: UIPageViewController,
+                            viewControllerBefore viewController: UIViewController) -> UIViewController? {
+        return nil
+    }
+
+    func pageViewController(_ pageViewController: UIPageViewController,
+                            viewControllerAfter viewController: UIViewController) -> UIViewController? {
+        return nil
     }
 }
