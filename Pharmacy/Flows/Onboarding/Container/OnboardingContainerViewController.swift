1e8d6c42ff24b79f22b3c7c2e9b8713db303a72b
Sapa Denys <denys.sapa.sbit@gmail.com>
2020-09-02 15:29:01 +0300

    Create Onboarding container view controller

---
+++ Pharmacy/Flows/Onboarding/Container/OnboardingContainerViewController.swift
@@ -0,0 +1,16 @@
+//
+//  OnboardingContainerViewController.swift
+//  Pharmacy
+//
+//  Created by Sapa Denys on 02.09.2020.
+//  Copyright © 2020 pharmacy. All rights reserved.
+//
+
+import UIKit
+
+class OnboardingContainerViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
