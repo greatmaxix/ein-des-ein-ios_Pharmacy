c6128eb451bdd13ba27544971604e9df2e01d76b
Mishko <valisrevenge@gmail.com>
2020-09-02 11:56:54 +0300

    Added some show/hiding functionality to checkout.

--- Pharmacy/Flows/Product/CheckoutViewController/CheckoutViewController.swift
+++ Pharmacy/Flows/Product/CheckoutViewController/CheckoutViewController.swift
@@ -8,15 +8,23 @@
 
 import UIKit
 
+protocol CheckoutOutput: class {
+    
+}
+
 final class CheckoutViewController: UIViewController {
     
+    @IBOutlet private weak var scrollView: UIScrollView!
     @IBOutlet private weak var contactInfoLabel: UILabel!
     @IBOutlet private weak var contactInfoButton: UIButton!
     
+    @IBOutlet private weak var customerInfoView: UIView!
+    @IBOutlet private weak var editCustomerView: UIView!
+        
     @IBOutlet private weak var nameFilledLabel: UILabel!
     @IBOutlet private weak var phoneFilledLabel: UILabel!
     @IBOutlet private weak var emailFilledLabel: UILabel!
-    @IBOutlet private weak var customerInfoView: UIView!
+    @IBOutlet private weak var customerInfoBackground: UIView!
     
     @IBOutlet private weak var nameInputView: TextInputView!
     @IBOutlet private weak var phoneInputView: TextInputView!
@@ -30,9 +38,10 @@ final class CheckoutViewController: UIViewController {
     @IBOutlet private weak var deliveryLabel: UILabel!
     @IBOutlet private weak var pickupLabel: UILabel!
     
+    @IBOutlet private weak var deliveryAddresHeader: UIView!
     @IBOutlet private weak var deliveryAddressLabel: UILabel!
     @IBOutlet private weak var showDeliveryButton: UIButton!
-    @IBOutlet weak var filledAddresView: UIView!
+    @IBOutlet private weak var filledAddresView: UIView!
     
     @IBOutlet private weak var cityInputView: TextInputView!
     @IBOutlet private weak var streetInputView: TextInputView!
@@ -40,8 +49,9 @@ final class CheckoutViewController: UIViewController {
     @IBOutlet private weak var flatNumberInputView: TextInputView!
     
     @IBOutlet private weak var descriptionTextView: UITextView!
+    @IBOutlet private weak var descriptionPlaceholderLabel: UILabel!
     @IBOutlet private weak var farmacyInfoView: UIView!
-    @IBOutlet weak var descriptionBackgroundView: UIView!
+    @IBOutlet private weak var descriptionBackgroundView: UIView!
     
     @IBOutlet private weak var cityAddress: UILabel!
     @IBOutlet private weak var houseAddress: UILabel!
@@ -51,13 +61,18 @@ final class CheckoutViewController: UIViewController {
     @IBOutlet private weak var farmacyPhoneLabel: UILabel!
     @IBOutlet private weak var farmacyWorkTimeLabel: UILabel!
     @IBOutlet private weak var farmacyMapButton: UIButton!
+    @IBOutlet private weak var paymentBackgroundView: UIView!
+    @IBOutlet private weak var customerAddresEditView: UIView!
+    @IBOutlet private weak var customerAddressView: UIView!
     
     @IBOutlet private weak var paymentMethodLabel: UILabel!
     @IBOutlet private weak var paymentStackView: UIStackView!
-
+    @IBOutlet weak var paymentHeightConstr: NSLayoutConstraint!
+    
     @IBOutlet private weak var orderLabel: UILabel!
     @IBOutlet private weak var orderButton: UIButton!
     @IBOutlet private weak var medicineStackView: UIStackView!
+    @IBOutlet private weak var medicineHeightConstr: NSLayoutConstraint!
     
     @IBOutlet private weak var sumTitleLabel: UILabel!
     @IBOutlet private weak var discountTitleLabel: UILabel!
@@ -73,22 +88,27 @@ final class CheckoutViewController: UIViewController {
     
     var model: CheckoutInput!
 
+    // MARK: - Life cycle
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         setupUI()
         setupLocalization()
+        setupOrder()
+        descriptionTextView.delegate = self
     }
 
     func setupUI() {
         
         title = R.string.localize.checkoutTitle()
+        
         deliveryButton.layer.borderWidth = 2
         deliveryButton.layer.borderColor = R.color.welcomeBlue()?.cgColor
         deliveryButton.layer.cornerRadius = 8
-        customerInfoView.layer.cornerRadius = 10
-        farmacyInfoView.layer.cornerRadius = 10
-        filledAddresView.layer.cornerRadius = 10
+        customerInfoBackground.layer.cornerRadius = GUI.cornerRadius
+        farmacyInfoView.layer.cornerRadius = GUI.cornerRadius
+        filledAddresView.layer.cornerRadius = GUI.cornerRadius
         
         pickupButton.layer.borderWidth = 2
         pickupButton.layer.borderColor = R.color.textDarkBlue()?.cgColor
@@ -105,6 +125,12 @@ final class CheckoutViewController: UIViewController {
         streetInputView.contentType = .other
         houseInputView.contentType = .other
         flatNumberInputView.contentType = .other
+        
+        nameInputView.contentType = .name
+        phoneInputView.contentType = .phone
+        emailInputView.contentType = .email
+        
+        paymentBackgroundView.layer.cornerRadius = GUI.cornerRadius
     }
     
     func setupLocalization() {
@@ -125,6 +151,8 @@ final class CheckoutViewController: UIViewController {
         streetInputView.placeholder = R.string.localize.checkoutStreet()
         houseInputView.placeholder = R.string.localize.checkoutHouseNumber()
         flatNumberInputView.placeholder = R.string.localize.checkoutFlatNumber()
+        
+        descriptionPlaceholderLabel.text = R.string.localize.checkoutDescription()
         paymentMethodLabel.text = R.string.localize.checkoutPaymentType()
         customerAddressLabel.text = R.string.localize.checkoutDeliveryAddress()
         orderLabel.text = R.string.localize.checkoutOrderConsistence()
@@ -138,19 +166,87 @@ final class CheckoutViewController: UIViewController {
         applyButton.setTitle(R.string.localize.checkoutApplyPurchase(), for: .normal)
     }
     
+    private func setupOrder() {
+        
+        var paymentViewHeight: CGFloat = 0
+        for _ in 0..<3 {
+            if let paymentView: PaymentMethodView = R.nib.paymentMethodView(owner: nil) {
+                paymentStackView.addArrangedSubview(paymentView)
+                paymentViewHeight = paymentView.frame.height
+            }
+        }
+        paymentHeightConstr.constant = 3 * paymentViewHeight
+        
+        var medicineViewHeight: CGFloat = 0
+        for _ in 0..<3 {
+            if let medicineView: MedicineView = R.nib.medicineView(owner: nil) {
+                medicineStackView.addArrangedSubview(medicineView)
+                medicineViewHeight = medicineView.frame.height
+            }
+        }
+        medicineHeightConstr.constant = 3 * medicineViewHeight + 2 * 10
+    }
+    
+    // MARK: - Outlets
+    
     @IBAction private func selectDelivery(_ sender: UIButton) {
          
-         let selectedDelivery: Bool = sender == deliveryButton
+        let selectedDelivery: Bool = sender == deliveryButton
          
-         deliveryButton.layer.borderColor = selectedDelivery ?  R.color.welcomeBlue()?.cgColor : R.color.textDarkBlue()?.cgColor
-         deliveryButton.tintColor = selectedDelivery ?  R.color.welcomeBlue() : R.color.textDarkBlue()
-         deliveryLabel.textColor = selectedDelivery ?  R.color.welcomeBlue() : R.color.textDarkBlue()
-         deliveryLabel.font = selectedDelivery ? R.font.openSansSemiBold(size: 14) : R.font.openSansRegular(size: 14)
+        deliveryButton.layer.borderColor = selectedDelivery ?  R.color.welcomeBlue()?.cgColor : R.color.textDarkBlue()?.cgColor
+        deliveryButton.tintColor = selectedDelivery ?  R.color.welcomeBlue() : R.color.textDarkBlue()
+        deliveryLabel.textColor = selectedDelivery ?  R.color.welcomeBlue() : R.color.textDarkBlue()
+        deliveryLabel.font = selectedDelivery ? R.font.openSansSemiBold(size: 14) : R.font.openSansRegular(size: 14)
          
-         pickupButton.layer.borderColor = !selectedDelivery ?  R.color.welcomeBlue()?.cgColor : R.color.textDarkBlue()?.cgColor
-         pickupButton.tintColor = !selectedDelivery ?  R.color.welcomeBlue() : R.color.textDarkBlue()
-         pickupLabel.textColor = !selectedDelivery ?  R.color.welcomeBlue() : R.color.textDarkBlue()
-         pickupLabel.font = !selectedDelivery ? R.font.openSansSemiBold(size: 14) : R.font.openSansRegular(size: 14)
-     }
+        pickupButton.layer.borderColor = !selectedDelivery ?  R.color.welcomeBlue()?.cgColor : R.color.textDarkBlue()?.cgColor
+        pickupButton.tintColor = !selectedDelivery ?  R.color.welcomeBlue() : R.color.textDarkBlue()
+        pickupLabel.textColor = !selectedDelivery ?  R.color.welcomeBlue() : R.color.textDarkBlue()
+        pickupLabel.font = !selectedDelivery ? R.font.openSansSemiBold(size: 14) : R.font.openSansRegular(size: 14)
+        
+        customerAddresEditView.isHidden = !selectedDelivery
+        customerAddressView.isHidden = true
+        deliveryAddresHeader.isHidden = !selectedDelivery
+    }
     
+    @IBAction func editCustomerInfo(_ sender: UIButton) {
+        sender.isHidden = true
+        customerInfoView.isHidden = true
+        editCustomerView.isHidden = false
+    }
+    @IBAction func editFarmacyAddress(_ sender: UIButton) {
+        sender.isHidden = true
+    }
+    
+    @IBAction func editCustomerAddress(_ sender: UIButton) {
+        sender.isHidden = true
+        customerAddressView.isHidden = true
+        customerAddresEditView.isHidden = false
+    }
+    @IBAction func editOrder() {
+    }
+    @IBAction func promoCode() {
+    }
+    @IBAction func applyOrder() {
+    }
+    
+}
+
+extension CheckoutViewController: UITextViewDelegate {
+    
+    func textViewDidBeginEditing(_ textView: UITextView) {
+        descriptionPlaceholderLabel.isHidden = true
+    }
+    
+    func textViewDidEndEditing(_ textView: UITextView) {
+        
+        if textView.text == nil || textView.text == "" {
+            descriptionPlaceholderLabel.isHidden = false
+        }
+    }
+}
+
+fileprivate extension CheckoutViewController {
+    struct GUI {
+        static let cornerRadius: CGFloat = 10
+    }
 }
