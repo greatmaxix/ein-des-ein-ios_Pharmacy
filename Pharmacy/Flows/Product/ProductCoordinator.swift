140a797e1c936db50a71b612d28121863e71c656
Daniiar Erkinov <dan4111k1@gmail.com>
2021-05-31 17:49:42 +0600

    Set hidesBottomBarWhenPushed

--- Pharmacy/Flows/Product/ProductCoordinator.swift
+++ Pharmacy/Flows/Product/ProductCoordinator.swift
@@ -76,17 +76,14 @@ fileprivate extension ProductCoordinator {
     }
     
     func createMapFlow(medicineId: Int) {
-//        guard let vc = R.storyboard.product.mapViewController() else { return }
-//        let model = MapModel(parent: self, medicineId: medicineId)
-//        model.output = vc
-//        vc.model = model
-//        navigation.pushViewController(vc, animated: true)
         guard let vc = R.storyboard.product.pharmacyMapListContainerViewController() else { return }
         let model = PharmacyMapListContainerModel(parent: self, medicineId: medicineId)
         model.output = vc
         vc.model = model
+        vc.hidesBottomBarWhenPushed = true
         navigation.pushViewController(vc, animated: true)
     }
+    
     func openChat() {
         let vc = ChatCoordinator(parent: self, navigation: navigation).createFlow()
         navigation.pushViewController(vc, animated: true)
